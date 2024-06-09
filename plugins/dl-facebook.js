ea925f717eb0734aba8f4493601a535536ad9dff
ڪَباؘسِ丫:١⁶𝑘𝑎𝑝𝑎𝑠 <167807989+TOXIC-KDIS@users.noreply.github.com>
2024-06-10 00:53:34 +0300

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -8,7 +8,7 @@ if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :
   try {
     let result = await fg.fbdl(args[0]);
     let tex = `
-┌─⊷ *𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕*
+┌─⊷ *𝐊𝐀𝐁𝐒 𝐏𝐎𝐓*
 ▢ *${mssg.title}:* ${result.title}
 └───────────`;
     conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
