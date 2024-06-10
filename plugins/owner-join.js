77fd8a9eaddfbbc7a08f8b0ca92e888b064d6ecf
ڪَباؘسِ丫:١⁶𝑘𝑎𝑝𝑎𝑠 <167807989+TOXIC-KDIS@users.noreply.github.com>
2024-06-10 20:27:46 +0300

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn, text, usedPrefix, command, args, participants, i
  
   let name = m.sender 
   let [_, code] = text.match(linkRegex) || []
-  if (!args[0]) throw `✳️ Envie el link del Grupo\n\n 📌 Ejemplo:\n *${usedPrefix + command}* <linkwa> <dias>\n\n_el número son los días que el bot estará en el grupo_` 
-  if (!code) throw `✳️ Link inválido`
+  if (!args[0]) throw `✳️ارسل رابط المجموعه\n\n 📌 Ejemplo:\n *${usedPrefix + command}* <linkwa> <dias>\n\n_el número son los días que el bot estará en el grupo_` 
+  if (!code) throw `✳️ الرابط غير صالح`
   if (!args[1]) throw `📌 Falta el número de días\n\n Ejemplo:\n *${usedPrefix + command}* <linkwa> 2`
   if (isNaN(args[1])) throw `✳️ Solo número, que representa los días que el bot estará en el grupo!`
   let owbot = global.owner[1] 
