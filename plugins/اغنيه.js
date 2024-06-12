d22035b0a916224ce260b0a020c7d18951910ff3
ڪَباؘسِ丫:١⁶𝑘𝑎𝑝𝑎𝑠 <167807989+TOXIC-KDIS@users.noreply.github.com>
2024-06-12 19:51:52 +0300

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -12,7 +12,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   let vidInfo = await ytdl.getInfo(url);
   let { videoDetails: { title, thumbnails, lengthSeconds } } = vidInfo;
   let thumbnail = thumbnails[thumbnails.length - 1].url;
-  let wm = '♪ 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 ♪'; // Your bot's watermark
+  let wm = '♪بوت♪'; // Your bot's watermark
   m.react(rwait);
   const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
   const tmpDir = os.tmpdir();
